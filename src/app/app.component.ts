2faf199bf6c4b22f4d8da59471697c0721802483
vaishnavk2001 <vaishnavjanardhanan22@gmail.com>
2024-08-30 23:57:57 +0530

    adding new content in user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

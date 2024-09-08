e0afd3522768735f1e59cef9f035c19234cd0a0e
vaishnavk2001 <vaishnavjanardhanan22@gmail.com>
2024-09-08 14:48:27 +0530

    selected option in the sidebar highlighted

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,20 @@
 import { Component ,EventEmitter,Input,output,Output,signal} from '@angular/core';
 import { DUMMY_USERS } from '../Dummy-user';
 import { user } from './user.model';
+import { CommonModule } from '@angular/common';
 const random = Math.floor(Math.random() *DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   
   @Input({required:true}) user!:user;
-
+  @Input ({required:true}) selected!:boolean
   @Output() select = new EventEmitter<string>();
   // select =  output<string>()
   get imagePath(){

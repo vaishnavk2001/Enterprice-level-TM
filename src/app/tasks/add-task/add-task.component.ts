c8e604aa818ec8f304a4424a05134713b280b87e
vaishnavk2001 <vaishnavjanardhanan22@gmail.com>
2024-09-08 23:15:31 +0530

    form handling

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,16 +1,24 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
+  title:String = "";
+  summary:String = "";
+  due_date:String = "";
+
   @Output() close = new EventEmitter<void>();
   onCancel(){
     this.close.emit();
+  }
+  onSubmit(){
+    
   }
 
 }
